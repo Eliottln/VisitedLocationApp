f42c768537daa5d923f79cf49085024ae4e64f1d
Eliottln <eliott.le-normand@etu.univ-lyon1.fr>
2023-01-31 11:45:58 +0100

    Initial Commit

---
+++ VisitedLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisitedLocation
+//
+//  Created by lpiem on 31/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
